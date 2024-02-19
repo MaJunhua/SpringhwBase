800b7601676301e49ccb6266b6264ecd743b1c6d
none <none>
2024-02-19 15:17:38 -0800

    .

---
+++ src/main/java/com/example/book/Entity/Book.java
@@ -0,0 +1,18 @@
+package com.example.book.Entity;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Data
+public class Book {
+    private String name;
+    private Integer price;
+
+    public Book(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+}
